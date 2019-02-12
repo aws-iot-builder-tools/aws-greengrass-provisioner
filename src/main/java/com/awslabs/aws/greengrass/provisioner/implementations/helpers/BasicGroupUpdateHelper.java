aws-iot-builder-tools/aws-greengrass-provisioner

commit 486a6323574d683cd39b07ebbfbb2aa7b117247e

Initial support for Device Tester for Greengrass

src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicGroupUpdateHelper.java
@@ -65,27 +65,22 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
 
         if (updateArguments.addSubscription || updateArguments.removeSubscription) {
             addOrRemoveSubscription(updateArguments, optionalGroupInformation.get());
-            return null;
         }
 
         if (updateArguments.addDevice != null) {
             addDevice(updateArguments, optionalGroupInformation.get());
-            return null;
         }
 
         if (updateArguments.removeDevice != null) {
             removeDevice(updateArguments, optionalGroupInformation.get());
-            return null;
         }
 
         if (updateArguments.addFunction != null) {
             addFunction(updateArguments, optionalGroupInformation.get());
-            return null;
         }
 
         if (updateArguments.removeFunction != null) {
             removeFunction(updateArguments, optionalGroupInformation.get());
-            return null;
         }
 
         throw new RuntimeException("This should never happen.  This is a bug.");
@@ -135,10 +130,7 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
                 .subscriptionDefinitionVersionArn(newSubscriptionDefinitionVersionArn)
                 .build();
 
-        Consumer<? super Void> successHandler = (Consumer<Void>) aVoid -> {
-        };
-
-        createAndWaitForDeployment(groupId, newGroupVersion, successHandler);
+        createAndWaitForDeployment(groupId, newGroupVersion);
     }
 
     private void addDevice(UpdateArguments updateArguments, GroupInformation groupInformation) {
@@ -184,6 +176,7 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
 
         String newDeviceDefinitionVersionArn = greengrassHelper.createDeviceDefinitionAndVersion(ggVariables.getDeviceDefinitionName(groupName), devices);
 
+
         GroupVersion newGroupVersion = GroupVersion.builder()
                 .deviceDefinitionVersionArn(newDeviceDefinitionVersionArn)
                 .build();
@@ -193,14 +186,21 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
         createAndWaitForDeployment(groupId, newGroupVersion, successHandler);
     }
 
+    private void createAndWaitForDeployment(String groupId, GroupVersion newGroupVersion) {
+        createAndWaitForDeployment(groupId, newGroupVersion, null);
+    }
+
     private void createAndWaitForDeployment(String groupId, GroupVersion newGroupVersion, Consumer<? super Void> successHandler) {
         String groupVersionId = greengrassHelper.createGroupVersion(groupId, newGroupVersion);
 
+        if (successHandler == null) {
+            successHandler = (Consumer<Void>) aVoid -> {
+            };
+        }
+
         Try.of(() -> deploymentHelper.createAndWaitForDeployment(Optional.empty(), Optional.empty(), groupId, groupVersionId))
                 .onSuccess(successHandler)
-                .onFailure(throwable -> {
-                    throw new RuntimeException(throwable);
-                });
+                .get();
     }
 
     private void addFunction(UpdateArguments updateArguments, GroupInformation groupInformation) {
@@ -287,12 +287,12 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
                 .functionDefinitionVersionArn(newFunctionDefinitionVersionArn)
                 .build();
 
-        Consumer<? super Void> successHandler = (Consumer<Void>) aVoid -> {
-            log.info("Function removed [" + functionToDelete.functionArn() + "]");
-            lambdaHelper.deleteAlias(functionToDeleteArn);
-        };
+        createAndWaitForDeployment(groupId, newGroupVersion, nothing -> logFunctionRemovedAndDeleteLambdaAlias(functionToDelete, functionToDeleteArn));
+    }
 
-        createAndWaitForDeployment(groupId, newGroupVersion, successHandler);
+    private void logFunctionRemovedAndDeleteLambdaAlias(Function functionToDelete, String functionToDeleteArn) {
+        log.info("Function removed [" + functionToDelete.functionArn() + "]");
+        lambdaHelper.deleteAlias(functionToDeleteArn);
     }
 
     private List<Subscription> removeSubscriptions(GroupInformation groupInformation, String thingOrFunctionArn) {
@@ -374,10 +374,8 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
                 .subscriptionDefinitionVersionArn(newSubscriptionDefinitionVersionArn)
                 .build();
 
-        Consumer<? super Void> successHandler = (Consumer<Void>) aVoid -> {
-        };
-
-        createAndWaitForDeployment(groupId, newGroupVersion, successHandler);
+        // Do nothing extra if successful
+        createAndWaitForDeployment(groupId, newGroupVersion);
     }
 
     /**
@@ -442,38 +440,44 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
 
     private List<Subscription> getTargetMatches(String target, boolean exactTarget, List<Subscription> sourceMatches) {
         return sourceMatches.stream()
-                .filter(subscription -> {
-                    if (exactTarget) {
-                        return subscription.target().equals(target);
-                    }
-
-                    return subscription.target().endsWith(target);
-                })
+                .filter(subscription -> checkTarget(target, exactTarget, subscription))
                 .collect(Collectors.toList());
+    }
+
+    private boolean checkTarget(String target, boolean exactTarget, Subscription subscription) {
+        if (exactTarget) {
+            return subscription.target().equals(target);
+        }
+
+        return subscription.target().endsWith(target);
     }
 
     private List<Subscription> getSourceMatches(String source, boolean exactSource, List<Subscription> subjectMatches) {
         return subjectMatches.stream()
-                .filter(subscription -> {
-                    if (exactSource) {
-                        return subscription.source().equals(source);
-                    }
-
-                    return subscription.source().endsWith(source);
-                })
+                .filter(subscription -> matchSource(source, exactSource, subscription))
                 .collect(Collectors.toList());
+    }
+
+    private boolean matchSource(String source, boolean exactSource, Subscription subscription) {
+        if (exactSource) {
+            return subscription.source().equals(source);
+        }
+
+        return subscription.source().endsWith(source);
     }
 
     private List<Subscription> getSubjectMatches(List<Subscription> subscriptions, Optional<String> optionalSubject) {
         return subscriptions.stream()
-                .filter(subscription -> {
-                    if (optionalSubject.isPresent()) {
-                        return subscription.subject().equals(optionalSubject.get());
-                    }
-
-                    return true;
-                })
+                .filter(subscription -> checkSubject(optionalSubject, subscription))
                 .collect(Collectors.toList());
+    }
+
+    private boolean checkSubject(Optional<String> optionalSubject, Subscription subscription) {
+        if (optionalSubject.isPresent()) {
+            return subscription.subject().equals(optionalSubject.get());
+        }
+
+        return true;
     }
 
     private Optional<String> validateSubscriptionSourceOrTarget(String sourceOrTarget, List<Function> functions, List<Device> devices) {

src/main/java/com/awslabs/aws/greengrass/provisioner/AwsGreengrassProvisioner.java
@@ -1,6 +1,7 @@
 package com.awslabs.aws.greengrass.provisioner;
 
-import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.*;
+import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.Operation;
+import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.SdkErrorHandler;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.ProvisionException;
@@ -9,20 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.core.exception.SdkClientException;
 
 import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 public class AwsGreengrassProvisioner implements Runnable {
-    public static final String serviceRoleName = "Greengrass_ServiceRole";
     private static Optional<Injector> optionalInjector = Optional.empty();
     @Inject
-    GroupQueryHelper groupQueryHelper;
-    @Inject
-    GroupUpdateHelper groupUpdateHelper;
-    @Inject
-    DeploymentHelper deploymentHelper;
+    Set<Operation> operations;
     private String[] args;
 
     @Inject
@@ -32,26 +27,32 @@ public class AwsGreengrassProvisioner implements Runnable {
     public static void main(String[] args) {
         SdkErrorHandler sdkErrorHandler = getSdkErrorHandler();
 
-        Try.of(() -> {
-            AwsGreengrassProvisioner awsGreengrassProvisioner = getAwsGreengrassProvisioner();
+        Try.of(() -> runProvisioner(args))
+                .onFailure(throwable -> handleProvisionerFailure(sdkErrorHandler, throwable))
+                .get();
+    }
 
-            awsGreengrassProvisioner.setArgs(args);
+    public static void handleProvisionerFailure(SdkErrorHandler sdkErrorHandler, Throwable throwable) {
+        // Sometimes dependency injection exceptions mask the actual SDK exception
+        if ((throwable instanceof ProvisionException) && (throwable.getCause() instanceof SdkClientException)) {
+            throwable = throwable.getCause();
+        }
 
-            awsGreengrassProvisioner.run();
+        if (throwable instanceof SdkClientException) {
+            sdkErrorHandler.handleSdkError((SdkClientException) throwable);
+        }
 
-            return null;
-        }).onFailure(throwable -> {
-            // Sometimes dependency injection exceptions mask the actual SDK exception
-            if ((throwable instanceof ProvisionException) && (throwable.getCause() instanceof SdkClientException)) {
-                throwable = throwable.getCause();
-            }
+        throw new RuntimeException(throwable);
+    }
 
-            if (throwable instanceof SdkClientException) {
-                sdkErrorHandler.handleSdkError((SdkClientException) throwable);
-            }
+    public static Void runProvisioner(String[] args) {
+        AwsGreengrassProvisioner awsGreengrassProvisioner = getAwsGreengrassProvisioner();
 
-            throw new RuntimeException(throwable);
-        });
+        awsGreengrassProvisioner.setArgs(args);
+
+        awsGreengrassProvisioner.run();
+
+        return null;
     }
 
     public static SdkErrorHandler getSdkErrorHandler() {
@@ -71,23 +72,23 @@ public class AwsGreengrassProvisioner implements Runnable {
     }
 
     public void run() {
-        List<Operation> operations =
-                Arrays.asList(deploymentHelper, groupUpdateHelper, groupQueryHelper);
-
         Try.of(() -> operations.stream()
                 // Find an operation with arguments that match
                 .filter(operation -> operation.matches(args))
                 .findFirst()
                 // Execute the operation
-                .map(operation -> operation.execute(log, args)))
+                .map(operation -> operation.execute(args)))
                 // If the operation fails then log the error
                 .onFailure(throwable -> log.error(throwable.getMessage()))
                 // If the operation succeeds make sure the result isn't empty. An empty result means that nothing was done.
-                .onSuccess(success -> {
-                    if (!success.isPresent()) {
-                        log.error("No operation specified");
-                    }
-                });
+                .onSuccess(success -> logIfNoOperationSpecified(success))
+                .get();
+    }
+
+    public void logIfNoOperationSpecified(Optional<Boolean> success) {
+        if (!success.isPresent()) {
+            log.error("No operation specified");
+        }
     }
 
     public void setArgs(String[] args) {

src/main/java/com/awslabs/aws/greengrass/provisioner/AwsGreengrassProvisionerModule.java
@@ -7,12 +7,14 @@ import com.awslabs.aws.greengrass.provisioner.implementations.builders.BasicNode
 import com.awslabs.aws.greengrass.provisioner.implementations.builders.BasicPythonBuilder;
 import com.awslabs.aws.greengrass.provisioner.implementations.clientproviders.*;
 import com.awslabs.aws.greengrass.provisioner.implementations.helpers.*;
+import com.awslabs.aws.greengrass.provisioner.interfaces.ExceptionHelper;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.GradleBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.MavenBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.NodeBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.PythonBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.*;
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
 import com.spotify.docker.client.ProgressHandler;
 import software.amazon.awssdk.regions.providers.AwsRegionProviderChain;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
@@ -61,25 +63,40 @@ public class AwsGreengrassProvisionerModule extends AbstractModule {
         bind(FunctionHelper.class).to(BasicFunctionHelper.class);
         bind(ArchiveHelper.class).to(BasicArchiveHelper.class);
         bind(GGDHelper.class).to(BasicGGDHelper.class);
-        bind(DeploymentHelper.class).to(BasicDeploymentHelper.class);
         bind(SubscriptionHelper.class).to(BasicSubscriptionHelper.class);
         bind(GlobalDefaultHelper.class).to(BasicGlobalDefaultHelper.class);
         bind(CloudFormationHelper.class).to(BasicCloudFormationHelper.class);
-        bind(DeploymentArgumentHelper.class).to(BasicDeploymentArgumentHelper.class);
-        bind(UpdateArgumentHelper.class).to(BasicUpdateArgumentHelper.class);
-        bind(QueryArgumentHelper.class).to(BasicQueryArgumentHelper.class);
         bind(LoggingHelper.class).to(BasicLoggingHelper.class);
         bind(EnvironmentHelper.class).to(BasicEnvironmentHelper.class);
         bind(ExecutorHelper.class).to(SingleThreadedExecutorHelper.class);
         //bind(ExecutorHelper.class).to(ParallelExecutorHelper.class);
 
+        // Argument helpers
+        bind(DeploymentArgumentHelper.class).to(BasicDeploymentArgumentHelper.class);
+        bind(UpdateArgumentHelper.class).to(BasicUpdateArgumentHelper.class);
+        bind(QueryArgumentHelper.class).to(BasicQueryArgumentHelper.class);
+        bind(TestArgumentHelper.class).to(BasicTestArgumentHelper.class);
+
         // Centralized error handling for SDK errors
         bind(SdkErrorHandler.class).to(BasicSdkErrorHandler.class);
 
         bind(IdExtractor.class).to(BasicIdExtractor.class);
-        bind(GroupQueryHelper.class).to(BasicGroupQueryHelper.class);
-        bind(GroupUpdateHelper.class).to(BasicGroupUpdateHelper.class);
         bind(ThreadHelper.class).to(BasicThreadHelper.class);
         bind(ProgressHandler.class).to(BasicProgressHandler.class);
+
+        Multibinder<Operation> operationMultibinder = Multibinder.newSetBinder(binder(), Operation.class);
+        operationMultibinder.addBinding().to(BasicDeploymentHelper.class);
+        operationMultibinder.addBinding().to(BasicGroupQueryHelper.class);
+        operationMultibinder.addBinding().to(BasicGroupUpdateHelper.class);
+        operationMultibinder.addBinding().to(BasicGroupTestHelper.class);
+
+        bind(DeploymentHelper.class).to(BasicDeploymentHelper.class);
+        bind(GroupQueryHelper.class).to(BasicGroupQueryHelper.class);
+        bind(GroupUpdateHelper.class).to(BasicGroupUpdateHelper.class);
+        bind(GroupTestHelper.class).to(BasicGroupTestHelper.class);
+
+        bind(DeviceTesterHelper.class).to(BasicDeviceTesterHelper.class);
+
+        bind(ExceptionHelper.class).to(BasicExceptionHelper.class);
     }
 }

src/main/java/com/awslabs/aws/greengrass/provisioner/data/DeviceTesterLogMessageType.java
@@ -1,0 +1,57 @@
+package com.awslabs.aws.greengrass.provisioner.data;
+
+import java.util.function.Function;
+
+public enum DeviceTesterLogMessageType {
+    CHECKING_GGC_VERSION(string -> string.equals("Checking whether version of Greengrass release is correct...")),
+    RUNNING(string -> string.equals("Running test case...")),
+    FINISHED(string -> string.equals("Finished running test case...")),
+    PASS(string -> string.equals("PASS")),
+    STARTING(string -> string.equals("Starting Greengrass...")),
+    START(string -> string.equals("start Greengrass executed successfully.")),
+    STOPPING(string -> string.equals("Stopping Greengrass...")),
+    STOP(string -> string.equals("stop Greengrass executed successfully.")),
+    PROVISIONING(string -> string.equals("Provisioning Greengrass...")),
+    FINISHED_PROVISIONING(string -> string.equals("Finished provisioning Greengrass.")),
+    CREATING_GGD(string -> string.equals("Creating GGD...")),
+    FINISHED_CREATING_GGD(string -> string.equals("Finished creating GGD...")),
+    PROVISIONING_GGD(string -> string.equals("Provisioning GGD...")),
+    FINISHED_PROVISIONING_GGD(string -> string.equals("Finished provisioning GGD.")),
+    CLEANING_UP(string -> string.equals("Cleaning up resources...")),
+    FAIL_WITHOUT_DURATION(string -> string.equals("FAIL")),
+    DEPLOYING_GROUP(string -> string.equals("Deploying group onto Greengrass core...")),
+    CREATING_GREENGRASS_LAMBDAS(string -> string.equals("Creating Greengrass Lambda(s)...")),
+    FINISHED_CREATING_GREENGRASS_LAMBDAS(string -> string.equals("Finished creating Greengrass Lambda(s).")),
+    CREATING_GREENGRASS_GROUP(string -> string.equals("Creating a Greengrass group...")),
+    FINISHED_DEPLOYING_GROUP(string -> string.equals("Finished deploying group onto Greengrass core.")),
+    RESTARTING_GREENGRASS(string -> string.equals("Restarting Greengrass...")),
+    RESTARTING_GREENGRASS_SUCCESSFUL(string -> string.equals("restart Greengrass executed successfully.")),
+    ERRORS_WHEN_CLEANING_UP_RESOURCES(string -> string.equals("Errors when cleaning up resources: ")),
+    RUNNING_GREENGRASS_ALREADY_INSTALLED(string -> string.equals("Running test with Greengrass already installed on your device at /greengrass...")),
+    COULD_NOT_FIND_GREENGRASS_RELEASE(string-> string.equals("Could not find Greengrass release in the location provided \"/greengrass\" on device under test. Please confirm that the correct location was provided.")),
+    // Partial matches
+    FAIL_WITH_DURATION(string -> string.startsWith("--- FAIL: ")),
+    ALL_TESTS_FINISHED(string -> string.startsWith(Constants.ALL_TESTS_FINISHED_MESSAGE)),
+    REPORT_GENERATED(string -> string.startsWith(Constants.REPORT_GENERATED_MESSAGE)),
+    TEST_TIMED_OUT(string -> string.startsWith("Test timed out")),
+    TIMED_OUT(string -> string.startsWith("Timed out")),
+    UNKNOWN_FAILURE(string -> string.startsWith("Failing")),
+    XML_SYNTAX_ERROR(string -> string.startsWith("XML syntax error")),
+    CLEANING_UP_RESOURCES_FAILED(string -> string.startsWith("Cleaning ") && string.contains("failed with error")),
+    STATUS_CODE_ERROR(string -> string.contains("status code") && string.contains("request id"));
+
+    private final Function<String, Boolean> matcher;
+
+    DeviceTesterLogMessageType(Function<String, Boolean> matcher) {
+        this.matcher = matcher;
+    }
+
+    public boolean matches(String string) {
+        return matcher.apply(string);
+    }
+
+    public static class Constants {
+        public static final String ALL_TESTS_FINISHED_MESSAGE = "All tests finished. Aggregated report generated at the path: ";
+        public static final String REPORT_GENERATED_MESSAGE = "AWS IoT Device Tester report generated at the path: ";
+    }
+}

src/main/java/com/awslabs/aws/greengrass/provisioner/data/arguments/Arguments.java
@@ -3,6 +3,8 @@ package com.awslabs.aws.greengrass.provisioner.data.arguments;
 public abstract class Arguments {
     final String LONG_GROUP_NAME_OPTION = "--group-name";
     final String SHORT_GROUP_NAME_OPTION = "-g";
+    final String LONG_ARCHITECTURE_OPTION = "--arch";
+    final String SHORT_ARCHITECTURE_OPTION = "-a";
 
     abstract public String getRequiredOptionName();
 

src/main/java/com/awslabs/aws/greengrass/provisioner/data/arguments/DeploymentArguments.java
@@ -8,9 +8,7 @@ public class DeploymentArguments extends Arguments {
     private final String SHORT_DEPLOYMENT_CONFIG_OPTION = "-d";
     @Getter
     private final String requiredOptionName = SHORT_DEPLOYMENT_CONFIG_OPTION;
-    private final String LONG_ARCHITECTURE_OPTION = "--arch";
-    private final String SHORT_ARCHITECTURE_OPTION = "-a";
-    private final String LONG_BUILD_CONTAINER_OPTION = "--build-container";
+   private final String LONG_BUILD_CONTAINER_OPTION = "--build-container";
     private final String SHORT_BUILD_CONTAINER_OPTION = "-c";
     private final String LONG_PUSH_CONTAINER_OPTION = "--push-container";
     private final String SHORT_PUSH_CONTAINER_OPTION = "-p";

src/main/java/com/awslabs/aws/greengrass/provisioner/data/arguments/TestArguments.java
@@ -1,0 +1,53 @@
+package com.awslabs.aws.greengrass.provisioner.data.arguments;
+
+import com.awslabs.aws.greengrass.provisioner.data.Architecture;
+import com.beust.jcommander.Parameter;
+import lombok.Getter;
+
+public class TestArguments extends Arguments {
+    private final String LONG_TEST_GROUP_OPTION = "--test-group";
+    @Getter
+    private final String requiredOptionName = LONG_TEST_GROUP_OPTION;
+    private final String LONG_USER_OPTION = "--user";
+    private final String SHORT_USER_OPTION = "-u";
+    private final String LONG_PRIVATE_KEY_PATH_OPTION = "--private-key-path";
+    private final String SHORT_PRIVATE_KEY_PATH_OPTION = "-k";
+    private final String LONG_DUT_OPTION = "--device-under-test";
+    private final String SHORT_DUT_OPTION = "--dut";
+    private final String LONG_CLEAN_OPTION = "--clean";
+    private final String LONG_GENERATE_CONFIG_OPTION = "--generate-config";
+    private final String LONG_DEVICE_TESTER_LOCATION_OPTION = "--device-tester-location";
+    private final String SHORT_DEVICE_TESTER_LOCATION_OPTION = "--dtl";
+    private final String LONG_OUTPUT_DIRECTORY_OPTION = "--output";
+    private final String SHORT_OUTPUT_DIRECTORY_OPTION = "-o";
+    @Parameter(names = {LONG_TEST_GROUP_OPTION}, description = "Use Device Tester on a device")
+    public boolean testGroup;
+
+    @Parameter(names = {LONG_ARCHITECTURE_OPTION, SHORT_ARCHITECTURE_OPTION}, description = "Architecture (X86_64, ARM32, ARM64)")
+    public String architectureString;
+    public Architecture architecture;
+    @Parameter(names = {LONG_USER_OPTION, SHORT_USER_OPTION}, description = "The username to use for SSH")
+    public String user;
+    @Parameter(names = {LONG_PRIVATE_KEY_PATH_OPTION, SHORT_PRIVATE_KEY_PATH_OPTION}, description = "The path to the private key for SSH")
+    public String privateKeyPath;
+    @Parameter(names = {LONG_DUT_OPTION, SHORT_DUT_OPTION}, description = "Hostname or the IP of the device under test")
+    public String deviceUnderTest;
+    @Parameter(names = {LONG_GROUP_NAME_OPTION, SHORT_GROUP_NAME_OPTION}, description = "The name of the Greengrass group")
+    public String groupName;
+    @Parameter(names = {LONG_DEVICE_TESTER_LOCATION_OPTION, SHORT_DEVICE_TESTER_LOCATION_OPTION}, description = "The location of the Device Tester directory or ZIP file")
+    public String deviceTesterLocation;
+    @Parameter(names = {LONG_CLEAN_OPTION}, description = "Cleans the /var/lib/GGQ directory")
+    public boolean clean;
+    @Parameter(names = {LONG_GENERATE_CONFIG_OPTION}, description = "Generates the config in the /var/lib/GGQ, required for HSI testing")
+    public boolean generateConfig;
+    @Parameter(names = {LONG_OUTPUT_DIRECTORY_OPTION, SHORT_OUTPUT_DIRECTORY_OPTION}, description = "The directory to place the test results in")
+    public String outputDirectory;
+    @Parameter(names = "--help", help = true)
+    @Getter
+    public boolean help;
+
+    @Override
+    public boolean isRequiredOptionSet() {
+        return testGroup;
+    }
+}

commit 33c79c373d41a49bfa697b80a1c364fb572e9cfc

Fixed missing return null regression from commit 486a6323574d683cd39b07ebbfbb2aa7b117247e

src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicGroupUpdateHelper.java
@@ -65,22 +65,27 @@ public class BasicGroupUpdateHelper implements GroupUpdateHelper {
 
         if (updateArguments.addSubscription || updateArguments.removeSubscription) {
             addOrRemoveSubscription(updateArguments, optionalGroupInformation.get());
+            return null;
         }
 
         if (updateArguments.addDevice != null) {
             addDevice(updateArguments, optionalGroupInformation.get());
+            return null;
         }
 
         if (updateArguments.removeDevice != null) {
             removeDevice(updateArguments, optionalGroupInformation.get());
+            return null;
         }
 
         if (updateArguments.addFunction != null) {
             addFunction(updateArguments, optionalGroupInformation.get());
+            return null;
         }
 
         if (updateArguments.removeFunction != null) {
             removeFunction(updateArguments, optionalGroupInformation.get());
+            return null;
         }
 
         throw new RuntimeException("This should never happen.  This is a bug.");