aws-iot-builder-tools/aws-greengrass-provisioner

commit 486a6323574d683cd39b07ebbfbb2aa7b117247e

Initial support for Device Tester for Greengrass

src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicGreengrassHelper.java
@@ -362,7 +362,7 @@ public class BasicGreengrassHelper implements GreengrassHelper {
 
         // Scrub the necessary functions
         Set<Function> scrubbedFunctions = functionsToScrubBuilder.build().stream()
-                .map(scrubFunctionForNoContainer())
+                .map(getScrubFunctionForNoContainerFunction())
                 .collect(Collectors.toSet());
 
         // Merge the functions back together (scrubbed functions and the functions we don't need to scrub)
@@ -383,17 +383,19 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         return createFunctionDefinitionResponse.latestVersionArn();
     }
 
-    private java.util.function.Function<Function, Function> scrubFunctionForNoContainer() {
-        return function -> {
-            Function.Builder functionBuilder = function.toBuilder();
-            FunctionConfiguration.Builder functionConfigurationBuilder = function.functionConfiguration().toBuilder();
-            functionConfigurationBuilder.memorySize(null);
-            functionBuilder.functionConfiguration(functionConfigurationBuilder.build());
+    private java.util.function.Function<Function, Function> getScrubFunctionForNoContainerFunction() {
+        return function -> scrubFunctionForNoContainer(function);
+    }
 
-            log.warn("Scrubbing memory size from function [" + function.functionArn() + "] since it is running without Greengrass container isolation");
+    private Function scrubFunctionForNoContainer(Function function) {
+        Function.Builder functionBuilder = function.toBuilder();
+        FunctionConfiguration.Builder functionConfigurationBuilder = function.functionConfiguration().toBuilder();
+        functionConfigurationBuilder.memorySize(null);
+        functionBuilder.functionConfiguration(functionConfigurationBuilder.build());
 
-            return functionBuilder.build();
-        };
+        log.warn("Scrubbing memory size from function [" + function.functionArn() + "] since it is running without Greengrass container isolation");
+
+        return functionBuilder.build();
     }
 
 
@@ -843,9 +845,7 @@ public class BasicGreengrassHelper implements GreengrassHelper {
     @Override
     public Device getDevice(String thingName) {
         Try.of(() -> iotHelper.getThingArn(thingName))
-                .recover(ResourceNotFoundException.class, throwable -> {
-                    throw new RuntimeException("Thing [" + thingName + "] does not exist");
-                })
+                .recover(ResourceNotFoundException.class, throwable -> rethrowResourceNotFoundException(thingName))
                 .get();
 
         String certificateArn = iotHelper.getThingPrincipal(thingName);
@@ -860,6 +860,16 @@ public class BasicGreengrassHelper implements GreengrassHelper {
                 .syncShadow(true)
                 .thingArn(iotHelper.getThingArn(thingName))
                 .build();
+    }
+
+    /**
+     * Rethrows the exception with a more succinct message
+     *
+     * @param thingName
+     * @return
+     */
+    public String rethrowResourceNotFoundException(String thingName) {
+        throw new RuntimeException("Thing [" + thingName + "] does not exist");
     }
 
     @Override
@@ -908,9 +918,7 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         GetFunctionDefinitionVersionResponse getFunctionDefinitionVersionResponse = greengrassClient.getFunctionDefinitionVersion(getFunctionDefinitionVersionRequest);
 
         FunctionDefinitionVersion functionDefinition = getFunctionDefinitionVersionResponse.definition();
-
-        // The returned list is an unmodifiable list, copy it to an array list so callers can modify it
-        List<Function> functions = new ArrayList<>(functionDefinition.functions());
+        List<Function> functions = functionDefinition.functions();
 
         return functions;
     }
@@ -929,9 +937,7 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         GetDeviceDefinitionVersionResponse getDeviceDefinitionVersionResponse = greengrassClient.getDeviceDefinitionVersion(getDeviceDefinitionVersionRequest);
 
         DeviceDefinitionVersion deviceDefinition = getDeviceDefinitionVersionResponse.definition();
-
-        // The returned list is an unmodifiable list, copy it to an array list so callers can modify it
-        List<Device> devices = new ArrayList<>(deviceDefinition.devices());
+        List<Device> devices = deviceDefinition.devices();
 
         return devices;
     }
@@ -950,9 +956,7 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         GetSubscriptionDefinitionVersionResponse getSubscriptionDefinitionVersionResponse = greengrassClient.getSubscriptionDefinitionVersion(getSubscriptionDefinitionVersionRequest);
 
         SubscriptionDefinitionVersion subscriptionDefinition = getSubscriptionDefinitionVersionResponse.definition();
-
-        // The returned list is an unmodifiable list, copy it to an array list so callers can modify it
-        List<Subscription> subscriptions = new ArrayList<>(subscriptionDefinition.subscriptions());
+        List<Subscription> subscriptions = subscriptionDefinition.subscriptions();
 
         return subscriptions;
     }

src/main/java/com/awslabs/aws/greengrass/provisioner/AwsGreengrassProvisioner.java
@@ -1,6 +1,7 @@
 package com.awslabs.aws.greengrass.provisioner;
 
-import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.*;
+import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.Operation;
+import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.SdkErrorHandler;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.ProvisionException;
@@ -9,20 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.core.exception.SdkClientException;
 
 import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 public class AwsGreengrassProvisioner implements Runnable {
-    public static final String serviceRoleName = "Greengrass_ServiceRole";
     private static Optional<Injector> optionalInjector = Optional.empty();
     @Inject
-    GroupQueryHelper groupQueryHelper;
-    @Inject
-    GroupUpdateHelper groupUpdateHelper;
-    @Inject
-    DeploymentHelper deploymentHelper;
+    Set<Operation> operations;
     private String[] args;
 
     @Inject
@@ -32,26 +27,32 @@ public class AwsGreengrassProvisioner implements Runnable {
     public static void main(String[] args) {
         SdkErrorHandler sdkErrorHandler = getSdkErrorHandler();
 
-        Try.of(() -> {
-            AwsGreengrassProvisioner awsGreengrassProvisioner = getAwsGreengrassProvisioner();
+        Try.of(() -> runProvisioner(args))
+                .onFailure(throwable -> handleProvisionerFailure(sdkErrorHandler, throwable))
+                .get();
+    }
 
-            awsGreengrassProvisioner.setArgs(args);
+    public static void handleProvisionerFailure(SdkErrorHandler sdkErrorHandler, Throwable throwable) {
+        // Sometimes dependency injection exceptions mask the actual SDK exception
+        if ((throwable instanceof ProvisionException) && (throwable.getCause() instanceof SdkClientException)) {
+            throwable = throwable.getCause();
+        }
 
-            awsGreengrassProvisioner.run();
+        if (throwable instanceof SdkClientException) {
+            sdkErrorHandler.handleSdkError((SdkClientException) throwable);
+        }
 
-            return null;
-        }).onFailure(throwable -> {
-            // Sometimes dependency injection exceptions mask the actual SDK exception
-            if ((throwable instanceof ProvisionException) && (throwable.getCause() instanceof SdkClientException)) {
-                throwable = throwable.getCause();
-            }
+        throw new RuntimeException(throwable);
+    }
 
-            if (throwable instanceof SdkClientException) {
-                sdkErrorHandler.handleSdkError((SdkClientException) throwable);
-            }
+    public static Void runProvisioner(String[] args) {
+        AwsGreengrassProvisioner awsGreengrassProvisioner = getAwsGreengrassProvisioner();
 
-            throw new RuntimeException(throwable);
-        });
+        awsGreengrassProvisioner.setArgs(args);
+
+        awsGreengrassProvisioner.run();
+
+        return null;
     }
 
     public static SdkErrorHandler getSdkErrorHandler() {
@@ -71,23 +72,23 @@ public class AwsGreengrassProvisioner implements Runnable {
     }
 
     public void run() {
-        List<Operation> operations =
-                Arrays.asList(deploymentHelper, groupUpdateHelper, groupQueryHelper);
-
         Try.of(() -> operations.stream()
                 // Find an operation with arguments that match
                 .filter(operation -> operation.matches(args))
                 .findFirst()
                 // Execute the operation
-                .map(operation -> operation.execute(log, args)))
+                .map(operation -> operation.execute(args)))
                 // If the operation fails then log the error
                 .onFailure(throwable -> log.error(throwable.getMessage()))
                 // If the operation succeeds make sure the result isn't empty. An empty result means that nothing was done.
-                .onSuccess(success -> {
-                    if (!success.isPresent()) {
-                        log.error("No operation specified");
-                    }
-                });
+                .onSuccess(success -> logIfNoOperationSpecified(success))
+                .get();
+    }
+
+    public void logIfNoOperationSpecified(Optional<Boolean> success) {
+        if (!success.isPresent()) {
+            log.error("No operation specified");
+        }
     }
 
     public void setArgs(String[] args) {

src/main/java/com/awslabs/aws/greengrass/provisioner/AwsGreengrassProvisionerModule.java
@@ -7,12 +7,14 @@ import com.awslabs.aws.greengrass.provisioner.implementations.builders.BasicNode
 import com.awslabs.aws.greengrass.provisioner.implementations.builders.BasicPythonBuilder;
 import com.awslabs.aws.greengrass.provisioner.implementations.clientproviders.*;
 import com.awslabs.aws.greengrass.provisioner.implementations.helpers.*;
+import com.awslabs.aws.greengrass.provisioner.interfaces.ExceptionHelper;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.GradleBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.MavenBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.NodeBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.builders.PythonBuilder;
 import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.*;
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
 import com.spotify.docker.client.ProgressHandler;
 import software.amazon.awssdk.regions.providers.AwsRegionProviderChain;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
@@ -61,25 +63,40 @@ public class AwsGreengrassProvisionerModule extends AbstractModule {
         bind(FunctionHelper.class).to(BasicFunctionHelper.class);
         bind(ArchiveHelper.class).to(BasicArchiveHelper.class);
         bind(GGDHelper.class).to(BasicGGDHelper.class);
-        bind(DeploymentHelper.class).to(BasicDeploymentHelper.class);
         bind(SubscriptionHelper.class).to(BasicSubscriptionHelper.class);
         bind(GlobalDefaultHelper.class).to(BasicGlobalDefaultHelper.class);
         bind(CloudFormationHelper.class).to(BasicCloudFormationHelper.class);
-        bind(DeploymentArgumentHelper.class).to(BasicDeploymentArgumentHelper.class);
-        bind(UpdateArgumentHelper.class).to(BasicUpdateArgumentHelper.class);
-        bind(QueryArgumentHelper.class).to(BasicQueryArgumentHelper.class);
         bind(LoggingHelper.class).to(BasicLoggingHelper.class);
         bind(EnvironmentHelper.class).to(BasicEnvironmentHelper.class);
         bind(ExecutorHelper.class).to(SingleThreadedExecutorHelper.class);
         //bind(ExecutorHelper.class).to(ParallelExecutorHelper.class);
 
+        // Argument helpers
+        bind(DeploymentArgumentHelper.class).to(BasicDeploymentArgumentHelper.class);
+        bind(UpdateArgumentHelper.class).to(BasicUpdateArgumentHelper.class);
+        bind(QueryArgumentHelper.class).to(BasicQueryArgumentHelper.class);
+        bind(TestArgumentHelper.class).to(BasicTestArgumentHelper.class);
+
         // Centralized error handling for SDK errors
         bind(SdkErrorHandler.class).to(BasicSdkErrorHandler.class);
 
         bind(IdExtractor.class).to(BasicIdExtractor.class);
-        bind(GroupQueryHelper.class).to(BasicGroupQueryHelper.class);
-        bind(GroupUpdateHelper.class).to(BasicGroupUpdateHelper.class);
         bind(ThreadHelper.class).to(BasicThreadHelper.class);
         bind(ProgressHandler.class).to(BasicProgressHandler.class);
+
+        Multibinder<Operation> operationMultibinder = Multibinder.newSetBinder(binder(), Operation.class);
+        operationMultibinder.addBinding().to(BasicDeploymentHelper.class);
+        operationMultibinder.addBinding().to(BasicGroupQueryHelper.class);
+        operationMultibinder.addBinding().to(BasicGroupUpdateHelper.class);
+        operationMultibinder.addBinding().to(BasicGroupTestHelper.class);
+
+        bind(DeploymentHelper.class).to(BasicDeploymentHelper.class);
+        bind(GroupQueryHelper.class).to(BasicGroupQueryHelper.class);
+        bind(GroupUpdateHelper.class).to(BasicGroupUpdateHelper.class);
+        bind(GroupTestHelper.class).to(BasicGroupTestHelper.class);
+
+        bind(DeviceTesterHelper.class).to(BasicDeviceTesterHelper.class);
+
+        bind(ExceptionHelper.class).to(BasicExceptionHelper.class);
     }
 }

src/main/java/com/awslabs/aws/greengrass/provisioner/data/DeviceTesterLogMessageType.java
@@ -1,0 +1,57 @@
+package com.awslabs.aws.greengrass.provisioner.data;
+
+import java.util.function.Function;
+
+public enum DeviceTesterLogMessageType {
+    CHECKING_GGC_VERSION(string -> string.equals("Checking whether version of Greengrass release is correct...")),
+    RUNNING(string -> string.equals("Running test case...")),
+    FINISHED(string -> string.equals("Finished running test case...")),
+    PASS(string -> string.equals("PASS")),
+    STARTING(string -> string.equals("Starting Greengrass...")),
+    START(string -> string.equals("start Greengrass executed successfully.")),
+    STOPPING(string -> string.equals("Stopping Greengrass...")),
+    STOP(string -> string.equals("stop Greengrass executed successfully.")),
+    PROVISIONING(string -> string.equals("Provisioning Greengrass...")),
+    FINISHED_PROVISIONING(string -> string.equals("Finished provisioning Greengrass.")),
+    CREATING_GGD(string -> string.equals("Creating GGD...")),
+    FINISHED_CREATING_GGD(string -> string.equals("Finished creating GGD...")),
+    PROVISIONING_GGD(string -> string.equals("Provisioning GGD...")),
+    FINISHED_PROVISIONING_GGD(string -> string.equals("Finished provisioning GGD.")),
+    CLEANING_UP(string -> string.equals("Cleaning up resources...")),
+    FAIL_WITHOUT_DURATION(string -> string.equals("FAIL")),
+    DEPLOYING_GROUP(string -> string.equals("Deploying group onto Greengrass core...")),
+    CREATING_GREENGRASS_LAMBDAS(string -> string.equals("Creating Greengrass Lambda(s)...")),
+    FINISHED_CREATING_GREENGRASS_LAMBDAS(string -> string.equals("Finished creating Greengrass Lambda(s).")),
+    CREATING_GREENGRASS_GROUP(string -> string.equals("Creating a Greengrass group...")),
+    FINISHED_DEPLOYING_GROUP(string -> string.equals("Finished deploying group onto Greengrass core.")),
+    RESTARTING_GREENGRASS(string -> string.equals("Restarting Greengrass...")),
+    RESTARTING_GREENGRASS_SUCCESSFUL(string -> string.equals("restart Greengrass executed successfully.")),
+    ERRORS_WHEN_CLEANING_UP_RESOURCES(string -> string.equals("Errors when cleaning up resources: ")),
+    RUNNING_GREENGRASS_ALREADY_INSTALLED(string -> string.equals("Running test with Greengrass already installed on your device at /greengrass...")),
+    COULD_NOT_FIND_GREENGRASS_RELEASE(string-> string.equals("Could not find Greengrass release in the location provided \"/greengrass\" on device under test. Please confirm that the correct location was provided.")),
+    // Partial matches
+    FAIL_WITH_DURATION(string -> string.startsWith("--- FAIL: ")),
+    ALL_TESTS_FINISHED(string -> string.startsWith(Constants.ALL_TESTS_FINISHED_MESSAGE)),
+    REPORT_GENERATED(string -> string.startsWith(Constants.REPORT_GENERATED_MESSAGE)),
+    TEST_TIMED_OUT(string -> string.startsWith("Test timed out")),
+    TIMED_OUT(string -> string.startsWith("Timed out")),
+    UNKNOWN_FAILURE(string -> string.startsWith("Failing")),
+    XML_SYNTAX_ERROR(string -> string.startsWith("XML syntax error")),
+    CLEANING_UP_RESOURCES_FAILED(string -> string.startsWith("Cleaning ") && string.contains("failed with error")),
+    STATUS_CODE_ERROR(string -> string.contains("status code") && string.contains("request id"));
+
+    private final Function<String, Boolean> matcher;
+
+    DeviceTesterLogMessageType(Function<String, Boolean> matcher) {
+        this.matcher = matcher;
+    }
+
+    public boolean matches(String string) {
+        return matcher.apply(string);
+    }
+
+    public static class Constants {
+        public static final String ALL_TESTS_FINISHED_MESSAGE = "All tests finished. Aggregated report generated at the path: ";
+        public static final String REPORT_GENERATED_MESSAGE = "AWS IoT Device Tester report generated at the path: ";
+    }
+}

src/main/java/com/awslabs/aws/greengrass/provisioner/data/arguments/Arguments.java
@@ -3,6 +3,8 @@ package com.awslabs.aws.greengrass.provisioner.data.arguments;
 public abstract class Arguments {
     final String LONG_GROUP_NAME_OPTION = "--group-name";
     final String SHORT_GROUP_NAME_OPTION = "-g";
+    final String LONG_ARCHITECTURE_OPTION = "--arch";
+    final String SHORT_ARCHITECTURE_OPTION = "-a";
 
     abstract public String getRequiredOptionName();
 

src/main/java/com/awslabs/aws/greengrass/provisioner/data/arguments/DeploymentArguments.java
@@ -8,9 +8,7 @@ public class DeploymentArguments extends Arguments {
     private final String SHORT_DEPLOYMENT_CONFIG_OPTION = "-d";
     @Getter
     private final String requiredOptionName = SHORT_DEPLOYMENT_CONFIG_OPTION;
-    private final String LONG_ARCHITECTURE_OPTION = "--arch";
-    private final String SHORT_ARCHITECTURE_OPTION = "-a";
-    private final String LONG_BUILD_CONTAINER_OPTION = "--build-container";
+   private final String LONG_BUILD_CONTAINER_OPTION = "--build-container";
     private final String SHORT_BUILD_CONTAINER_OPTION = "-c";
     private final String LONG_PUSH_CONTAINER_OPTION = "--push-container";
     private final String SHORT_PUSH_CONTAINER_OPTION = "-p";

src/main/java/com/awslabs/aws/greengrass/provisioner/data/arguments/TestArguments.java
@@ -1,0 +1,53 @@
+package com.awslabs.aws.greengrass.provisioner.data.arguments;
+
+import com.awslabs.aws.greengrass.provisioner.data.Architecture;
+import com.beust.jcommander.Parameter;
+import lombok.Getter;
+
+public class TestArguments extends Arguments {
+    private final String LONG_TEST_GROUP_OPTION = "--test-group";
+    @Getter
+    private final String requiredOptionName = LONG_TEST_GROUP_OPTION;
+    private final String LONG_USER_OPTION = "--user";
+    private final String SHORT_USER_OPTION = "-u";
+    private final String LONG_PRIVATE_KEY_PATH_OPTION = "--private-key-path";
+    private final String SHORT_PRIVATE_KEY_PATH_OPTION = "-k";
+    private final String LONG_DUT_OPTION = "--device-under-test";
+    private final String SHORT_DUT_OPTION = "--dut";
+    private final String LONG_CLEAN_OPTION = "--clean";
+    private final String LONG_GENERATE_CONFIG_OPTION = "--generate-config";
+    private final String LONG_DEVICE_TESTER_LOCATION_OPTION = "--device-tester-location";
+    private final String SHORT_DEVICE_TESTER_LOCATION_OPTION = "--dtl";
+    private final String LONG_OUTPUT_DIRECTORY_OPTION = "--output";
+    private final String SHORT_OUTPUT_DIRECTORY_OPTION = "-o";
+    @Parameter(names = {LONG_TEST_GROUP_OPTION}, description = "Use Device Tester on a device")
+    public boolean testGroup;
+
+    @Parameter(names = {LONG_ARCHITECTURE_OPTION, SHORT_ARCHITECTURE_OPTION}, description = "Architecture (X86_64, ARM32, ARM64)")
+    public String architectureString;
+    public Architecture architecture;
+    @Parameter(names = {LONG_USER_OPTION, SHORT_USER_OPTION}, description = "The username to use for SSH")
+    public String user;
+    @Parameter(names = {LONG_PRIVATE_KEY_PATH_OPTION, SHORT_PRIVATE_KEY_PATH_OPTION}, description = "The path to the private key for SSH")
+    public String privateKeyPath;
+    @Parameter(names = {LONG_DUT_OPTION, SHORT_DUT_OPTION}, description = "Hostname or the IP of the device under test")
+    public String deviceUnderTest;
+    @Parameter(names = {LONG_GROUP_NAME_OPTION, SHORT_GROUP_NAME_OPTION}, description = "The name of the Greengrass group")
+    public String groupName;
+    @Parameter(names = {LONG_DEVICE_TESTER_LOCATION_OPTION, SHORT_DEVICE_TESTER_LOCATION_OPTION}, description = "The location of the Device Tester directory or ZIP file")
+    public String deviceTesterLocation;
+    @Parameter(names = {LONG_CLEAN_OPTION}, description = "Cleans the /var/lib/GGQ directory")
+    public boolean clean;
+    @Parameter(names = {LONG_GENERATE_CONFIG_OPTION}, description = "Generates the config in the /var/lib/GGQ, required for HSI testing")
+    public boolean generateConfig;
+    @Parameter(names = {LONG_OUTPUT_DIRECTORY_OPTION, SHORT_OUTPUT_DIRECTORY_OPTION}, description = "The directory to place the test results in")
+    public String outputDirectory;
+    @Parameter(names = "--help", help = true)
+    @Getter
+    public boolean help;
+
+    @Override
+    public boolean isRequiredOptionSet() {
+        return testGroup;
+    }
+}

src/main/java/com/awslabs/aws/greengrass/provisioner/docker/AbstractDockerHelper.java
@@ -3,6 +3,7 @@ package com.awslabs.aws.greengrass.provisioner.docker;
 import com.awslabs.aws.greengrass.provisioner.data.Architecture;
 import com.awslabs.aws.greengrass.provisioner.docker.interfaces.DockerClientProvider;
 import com.awslabs.aws.greengrass.provisioner.docker.interfaces.DockerHelper;
+import com.awslabs.aws.greengrass.provisioner.interfaces.ExceptionHelper;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.ProgressHandler;
 import com.spotify.docker.client.exceptions.DockerException;
@@ -22,6 +23,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import static io.vavr.API.*;
+import static io.vavr.Predicates.instanceOf;
 
 @Slf4j
 public abstract class AbstractDockerHelper implements DockerHelper {
@@ -76,30 +80,44 @@ public abstract class AbstractDockerHelper implements DockerHelper {
 
     @Override
     public void createEcrRepositoryIfNecessary() {
-        Try.of(() -> {
-            DescribeRepositoriesResponse describeRepositoriesResponse = getEcrClient().describeRepositories(
-                    DescribeRepositoriesRequest.builder()
-                            .repositoryNames(ecrRepositoryName.get())
-                            .build());
-
-            if (describeRepositoriesResponse.repositories().size() == 1) {
-                log.info("ECR repository [" + ecrRepositoryName.get() + "] already exists");
-                return null;
-            }
-
-            log.info("More than one repository found that matched [" + ecrRepositoryName.get() + "], cannot continue");
-
-            throw new RuntimeException("More than one matching ECR repository");
-        })
-                .recover(RepositoryNotFoundException.class, throwable -> {
-                    log.info("Creating ECR repository [" + ecrRepositoryName.get() + "]");
-                    getEcrClient().createRepository(CreateRepositoryRequest.builder()
-                            .repositoryName(ecrRepositoryName.get())
-                            .build());
-
-                    return null;
-                })
+        Try.of(() -> describeRepositories())
+                .onFailure(throwable -> Match(throwable).of(
+                        Case($(instanceOf(RepositoryNotFoundException.class)), ignore -> createEcrRepository()),
+                        Case($(), this::rethrowAsRuntimeException)))
                 .get();
+    }
+
+    private Void describeRepositories() {
+        DescribeRepositoriesResponse describeRepositoriesResponse = getEcrClient().describeRepositories(
+                DescribeRepositoriesRequest.builder()
+                        .repositoryNames(ecrRepositoryName.get())
+                        .build());
+
+        if (describeRepositoriesResponse.repositories().size() == 1) {
+            log.info("ECR repository [" + ecrRepositoryName.get() + "] already exists");
+            return null;
+        }
+
+        log.info("More than one repository found that matched [" + ecrRepositoryName.get() + "], cannot continue");
+
+        throw new RuntimeException("More than one matching ECR repository");
+    }
+
+    private Void rethrowAsRuntimeException(Throwable throwable) {
+        getExceptionHelper().rethrowAsRuntimeException(throwable);
+
+        return null;
+    }
+
+    protected abstract ExceptionHelper getExceptionHelper();
+
+    public Void createEcrRepository() {
+        log.info("Creating ECR repository [" + ecrRepositoryName.get() + "]");
+        getEcrClient().createRepository(CreateRepositoryRequest.builder()
+                .repositoryName(ecrRepositoryName.get())
+                .build());
+
+        return null;
     }
 
     protected abstract EcrClient getEcrClient();
@@ -269,12 +287,17 @@ public abstract class AbstractDockerHelper implements DockerHelper {
         Container container = optionalContainer.get();
 
         Try.withResources(this::getDockerClient)
-                .of(dockerClient -> {
-                    dockerClient.stopContainer(container.id(), 5);
-                    return null;
-                })
-                .recover(DockerException.class, this::printFailedToStopContainerAndThrow)
-                .recover(InterruptedException.class, this::printFailedToStopContainerAndThrow);
+                .of(dockerClient -> stopContainer(container, dockerClient))
+                .onFailure(throwable -> Match(throwable).of(
+                        Case($(instanceOf(DockerException.class)), this::printFailedToStopContainerAndThrow),
+                        Case($(instanceOf(InterruptedException.class)), this::printFailedToStopContainerAndThrow),
+                        Case($(), this::rethrowAsRuntimeException)))
+                .get();
+    }
+
+    public Void stopContainer(Container container, DockerClient dockerClient) throws DockerException, InterruptedException {
+        dockerClient.stopContainer(container.id(), 5);
+        return null;
     }
 
     private Void printFailedToStopContainerAndThrow(Throwable throwable) {

commit e2abcbe836b6b18252eed005ab7e6ffa29426511

Fixed unmodifiable list regression from commit 486a6323574d683cd39b07ebbfbb2aa7b117247e

src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicGreengrassHelper.java
@@ -918,7 +918,9 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         GetFunctionDefinitionVersionResponse getFunctionDefinitionVersionResponse = greengrassClient.getFunctionDefinitionVersion(getFunctionDefinitionVersionRequest);
 
         FunctionDefinitionVersion functionDefinition = getFunctionDefinitionVersionResponse.definition();
-        List<Function> functions = functionDefinition.functions();
+
+        // The returned list is an unmodifiable list, copy it to an array list so callers can modify it
+        List<Function> functions = new ArrayList<>(functionDefinition.functions());
 
         return functions;
     }
@@ -937,7 +939,9 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         GetDeviceDefinitionVersionResponse getDeviceDefinitionVersionResponse = greengrassClient.getDeviceDefinitionVersion(getDeviceDefinitionVersionRequest);
 
         DeviceDefinitionVersion deviceDefinition = getDeviceDefinitionVersionResponse.definition();
-        List<Device> devices = deviceDefinition.devices();
+
+        // The returned list is an unmodifiable list, copy it to an array list so callers can modify it
+        List<Device> devices = new ArrayList<>(deviceDefinition.devices());
 
         return devices;
     }
@@ -956,7 +960,9 @@ public class BasicGreengrassHelper implements GreengrassHelper {
         GetSubscriptionDefinitionVersionResponse getSubscriptionDefinitionVersionResponse = greengrassClient.getSubscriptionDefinitionVersion(getSubscriptionDefinitionVersionRequest);
 
         SubscriptionDefinitionVersion subscriptionDefinition = getSubscriptionDefinitionVersionResponse.definition();
-        List<Subscription> subscriptions = subscriptionDefinition.subscriptions();
+
+        // The returned list is an unmodifiable list, copy it to an array list so callers can modify it
+        List<Subscription> subscriptions = new ArrayList<>(subscriptionDefinition.subscriptions());
 
         return subscriptions;
     }