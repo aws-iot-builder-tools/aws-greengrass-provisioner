fbac2a1da5d53c219bf1c15e3562fbc87b8ac15f
Tim Mattison <tim@mattison.org>
2019-05-02 06:16:03 -0400

    Build loop no longer can automatically push to master

--- testing/build-loop.sh
+++ testing/build-loop.sh
@@ -13,12 +13,19 @@ do
     # Simple mechanism to see if anything in the directory changed since the last run
     signature2=`find . -not -type d -ls | sort | shasum`
 
-    if [[ $signature1 != $signature2 ]] ; then
-        until time ./testing/push-to-dockerhub.sh
-    	do
-            echo "Build failed, retrying"
-            sleep 10
-        done
+    if [[ $signature1 != $signature2 ]]; then
+        TAG=`git symbolic-ref --short HEAD`
+
+	if [[ "$TAG" == "master" ]]; then
+            echo "Refusing to automatically build and push to master..."
+	    sleep 10
+        else
+            until time ./testing/push-to-dockerhub.sh
+        	do
+                echo "Build failed, retrying"
+                sleep 10
+            done
+        fi
         
         signature1=$signature2
     fi
