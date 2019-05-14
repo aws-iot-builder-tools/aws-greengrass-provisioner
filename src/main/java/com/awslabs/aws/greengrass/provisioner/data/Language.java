250ea28ae6701445e55bcce0bb6ae75bc83b68ba
Tim Mattison <tim@mattison.org>
2019-05-14 05:56:09 -0700

    Updates to use NodeJS 8.10.0 by default

--- src/main/java/com/awslabs/aws/greengrass/provisioner/data/Language.java
+++ src/main/java/com/awslabs/aws/greengrass/provisioner/data/Language.java
@@ -6,7 +6,7 @@ import software.amazon.awssdk.services.lambda.model.Runtime;
 public enum Language {
     Python(Runtime.PYTHON2_7), // Legacy, equivalent to PYTHON2_7
     Java(Runtime.JAVA8),       // Legacy, equivalent to JAVA8
-    Node(Runtime.NODEJS6_10),  // Legacy, equivalent to NODEJS6_10
+    Node(Runtime.NODEJS8_10),  // Legacy, equivalent to NODEJS8_10
     NODEJS6_10(Runtime.NODEJS6_10),
     NODEJS8_10(Runtime.NODEJS8_10),
     JAVA8(Runtime.JAVA8),
