fecc42d96c3876a684efc4d50868d71ec6c61f62
Tim Mattison <tim@mattison.org>
2019-05-14 06:29:21 -0700

    Not setting a default runtime for legacy options to make sure we always force it to the correct value

--- src/main/java/com/awslabs/aws/greengrass/provisioner/data/Language.java
+++ src/main/java/com/awslabs/aws/greengrass/provisioner/data/Language.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import software.amazon.awssdk.services.lambda.model.Runtime;
 
 public enum Language {
-    Python(Runtime.PYTHON2_7), // Legacy, equivalent to PYTHON2_7
-    Java(Runtime.JAVA8),       // Legacy, equivalent to JAVA8
-    Node(Runtime.NODEJS8_10),  // Legacy, equivalent to NODEJS8_10
+    Python(null), // Legacy
+    Java(null),   // Legacy
+    Node(null),   // Legacy
     NODEJS6_10(Runtime.NODEJS6_10),
     NODEJS8_10(Runtime.NODEJS8_10),
     JAVA8(Runtime.JAVA8),
