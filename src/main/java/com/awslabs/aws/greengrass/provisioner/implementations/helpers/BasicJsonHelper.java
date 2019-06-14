b14aec5a50efcf1e76e6fef3e757cc7f2e06ecb5
Tim Mattison <tim@mattison.org>
2019-06-13 21:32:25 -0400

    JSON serialization/deserialization support for Immutables

--- src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicJsonHelper.java
+++ src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicJsonHelper.java
@@ -1,10 +1,12 @@
 package com.awslabs.aws.greengrass.provisioner.implementations.helpers;
 
 import com.awslabs.aws.greengrass.provisioner.interfaces.helpers.JsonHelper;
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapterFactory;
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
+import java.util.ServiceLoader;
 
 public class BasicJsonHelper implements JsonHelper {
     @Inject
@@ -13,16 +15,27 @@ public class BasicJsonHelper implements JsonHelper {
 
     @Override
     public String toJson(Object object) {
-        return new GsonBuilder()
+        return getGsonBuilder()
                 .disableHtmlEscaping()
                 .setPrettyPrinting()
                 .create()
                 .toJson(object);
     }
 
+    @NotNull
+    private GsonBuilder getGsonBuilder() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        for (TypeAdapterFactory factory : ServiceLoader.load(TypeAdapterFactory.class)) {
+            gsonBuilder.registerTypeAdapterFactory(factory);
+        }
+
+        return gsonBuilder;
+    }
+
     @Override
     public <T> T fromJson(Class<T> clazz, byte[] json) {
-        return new Gson()
+        return getGsonBuilder().create()
                 .fromJson(new String(json), clazz);
     }
 }
