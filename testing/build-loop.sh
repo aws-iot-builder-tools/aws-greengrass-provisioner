c299a81a46a6af2e5394a3247b38cce720aee03f
Tim Mattison <tim@mattison.org>
2020-01-10 14:59:39 -0500

    Reformatted build loop script

--- testing/build-loop.sh
+++ testing/build-loop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CWD=`basename "$PWD"`
+CWD=$(basename "$PWD")
 
 if [ "$CWD" == "testing" ]; then
   cd ..
@@ -8,28 +8,26 @@ fi
 
 signature1=""
 
-while [[ true ]]
-do
-    # Simple mechanism to see if anything in the directory changed since the last run
-    signature2=`find . -not -type d -ls | sort | shasum`
+while [[ true ]]; do
+  # Simple mechanism to see if anything in the directory changed since the last run
+  signature2=$(find . -not -type d -ls | sort | shasum)
 
-    if [[ $signature1 != $signature2 ]]; then
-        TAG=`git symbolic-ref --short HEAD | tr -cd '[:alnum:]._-'`
+  if [[ $signature1 != $signature2 ]]; then
+    TAG=$(git symbolic-ref --short HEAD | tr -cd '[:alnum:]._-')
 
-	if [[ "$TAG" == "master" ]]; then
-            ./build-ggp-docker-container.sh
-            echo "Refusing to automatically push to master..."
-	    sleep 10
-        else
-            until time ./testing/push-to-dockerhub.sh $TAG
-        	do
-                echo "Build failed, retrying"
-                sleep 10
-            done
-        fi
-        
-        signature1=$signature2
+    if [[ "$TAG" == "master" ]]; then
+      ./build-ggp-docker-container.sh
+      echo "Refusing to automatically push to master..."
+      sleep 10
+    else
+      until time ./testing/push-to-dockerhub.sh $TAG; do
+        echo "Build failed, retrying"
+        sleep 10
+      done
     fi
 
-    sleep 1
+    signature1=$signature2
+  fi
+
+  sleep 1
 done
