36433ded7ba77ac34a6b86e5a60b937fc3d55732
Lei Xu <komushi@gmail.com>
2021-05-29 09:31:34 +0900

    extract-from-outfile.sh fix

--- lambda-support/extract-from-outfile.sh
+++ lambda-support/extract-from-outfile.sh
@@ -17,10 +17,10 @@ rm -rf certs config
 mkdir certs
 mkdir config
 
-jq -r '.["certs/core.crt"]' $OUTFILE > certs/core.crt
-jq -r '.["certs/core.key"]' $OUTFILE > certs/core.key
-jq -r '.["certs/root.ca.pem"]' $OUTFILE > certs/root.ca.pem
-jq -r '.["config/config.json"]' $OUTFILE > config/config.json
+jq -r '.["coreCrt"]' $OUTFILE > certs/core.crt
+jq -r '.["coreKey"]' $OUTFILE > certs/core.key
+jq -r '.["rootCaPem"]' $OUTFILE > certs/root.ca.pem
+jq -r '.["configJson"]' $OUTFILE > config/config.json
 
 tar cjvf $GROUP_NAME.tar.bz2 certs config
 rm -rf certs config
