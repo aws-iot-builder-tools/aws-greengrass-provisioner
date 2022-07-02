87118e9ed84fbd28bde910cd06328c29be59c369
Lei Xu <komushi@gmail.com>
2022-07-02 14:18:49 +0900

    readme added; waitfunction testing2

--- src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicLambdaHelper.java
+++ src/main/java/com/awslabs/aws/greengrass/provisioner/implementations/helpers/BasicLambdaHelper.java
@@ -229,7 +229,7 @@ public class BasicLambdaHelper implements LambdaHelper {
     @Override
     public LambdaFunctionArnInfo publishLambdaFunctionVersion(FunctionName functionName) {
 
-        waitFunction(functionName);
+        waitFunction(functionName, true);
 
         PublishVersionResponse publishVersionResponse = v2LambdaHelper.publishFunctionVersion(functionName);
 
